586af7b5126776fc94e7b9937eb84c99c3f4c214
Maruf Khan <marufatrai12@gmail.com>
2023-11-12 10:55:22 +0600

    JS-Class-3

---
+++ Javascript/script.js
@@ -0,0 +1,27 @@
+
+    // JavaScript Comment
+
+        // document.write("This Is Single line Comment.");
+
+       /*
+        document.write("Multi line Comment.");
+
+        document.write("Multi line Comment 2.");
+
+        document.write("Multi line Comment. 3");
+
+        */
+
+
+       // JavaScript DataType Function.
+
+
+    document.write(typeof("Hello"));  // it is String Data Type.
+
+    document.write("<br/>") // Line BreacK 
+
+    document.write(typeof(501)); // it is Number Data Type.
+
+    document.write("<br/>") // Line BreacK 
+
+    document.write(typeof(true)); // it is Boolean Data Type.
